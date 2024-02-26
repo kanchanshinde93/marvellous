3159f5dbf3b21f3e972e8e790bc61f89f035a377
kanchanshinde93 <kanchanshinde@fabloplatforms.com>
2024-02-26 17:31:42 +0530

    add

--- src/app/main/sample/about/about.component.ts
+++ src/app/main/sample/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
+  @ViewChildren('counterElement') counterElements: QueryList<ElementRef>;
   constructor() { }
 
   ngOnInit(): void {
   }
+  ngAfterViewInit(): void {
+    this.counterElements.forEach((counterElement: ElementRef) => {
+      this.animateCounter(counterElement.nativeElement);
+    });
+  }
+  animateCounter(counterElement: HTMLElement) {
+    let count = 0;
+    const targetCount = parseInt(counterElement.innerText);
+    const duration = 5000;
+    const step = Math.ceil(targetCount / (duration / 100)); // calculate step count based on duration
 
+    const timer = setInterval(() => {
+      count += step;
+      if (count >= targetCount) {
+        clearInterval(timer);
+        count = targetCount;
+      }
+      counterElement.innerText = count.toString();
+    }, 100); // interval set to 100ms
+  }
 }
--- src/app/main/sample/ourfeature/ourfeature.component.ts
+++ src/app/main/sample/ourfeature/ourfeature.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-ourfeature',
   templateUrl: './ourfeature.component.html',
   styleUrls: ['./ourfeature.component.scss']
 })
-export class OurfeatureComponent implements OnInit {
-
+export class OurfeatureComponent implements AfterViewInit  {
+  @ViewChildren('counterElement') counterElements: QueryList<ElementRef>;
   constructor() { }
 
   ngOnInit(): void {
+   
   }
 
+  ngAfterViewInit(): void {
+    this.counterElements.forEach((counterElement: ElementRef) => {
+      const textContent = counterElement.nativeElement.innerText;
+      const numberPart:any = parseInt(textContent);
+      const textPart:any = textContent.replace(numberPart.toString(), '');
+      this.animateCounter(counterElement.nativeElement, numberPart, textPart);
+    });
+  }
+  animateCounter(counterElement: HTMLElement, targetNumber: number, textPart: string) {
+    let count = 0;
+    const targetCount = parseInt(counterElement.innerText);
+    const duration = 5000;
+    const step = Math.ceil(targetCount / (duration / 100)); // calculate step count based on duration
+
+    const timer = setInterval(() => {
+      count += step;
+      if (count >= targetCount) {
+        clearInterval(timer);
+        count = targetCount;
+      }
+      counterElement.innerText = count.toString();
+    }, 100); // interval set to 100ms
+  }
 }
