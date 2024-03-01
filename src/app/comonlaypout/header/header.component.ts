2785949571307defc77cb3b4ff589d44497d1af7
kanchanshinde93 <kanchanshinde@fabloplatforms.com>
2024-03-01 18:32:42 +0530

    add top button

--- src/app/comonlaypout/header/header.component.ts
+++ src/app/comonlaypout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  scrollBreakpoint: number = window.innerHeight * 0.9;
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollOffset = window.scrollY;
+    const scrollButton = document.querySelector('.scroll-top');
 
+    if (scrollOffset >= this.scrollBreakpoint) {
+      scrollButton.classList.add('visible');
+    } else {
+      scrollButton.classList.remove('visible');
+    }
+  }
+
+  // Scroll to top when the button is clicked
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  // Method to check if the button should be visible
+  isButtonVisible(): boolean {
+    const scrollOffset = window.scrollY;
+    return scrollOffset >= this.scrollBreakpoint;
+  }
 }
